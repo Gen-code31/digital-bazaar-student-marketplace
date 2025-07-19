f90def27e42945b10e49feab2c3fa09f8e237dbd
Gen-code31 <hablaregen931@gmail.com>
2025-07-19 11:33:39 +0800

    Add .next/static/chunks/src_app_products_[id]_page_tsx_379fa8b3._.js 1752896018795

---
+++ .next/static/chunks/src_app_products_[id]_page_tsx_379fa8b3._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_148ac51f._.js"
+],
+    source: "dynamic"
+});
